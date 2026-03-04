a516e9ef3ad56ad69e467cc4d0a9b107f5071030
iammiguelsolis <miguelsoliscunza24@gmail.com>
2026-03-04 08:13:22 -0500

    fix

--- backend/src/patterns/observer/WhatsAppObserver.ts
+++ backend/src/patterns/observer/WhatsAppObserver.ts
@@ -3,8 +3,6 @@ import { IExpense } from '../../models/Expense';
 
 export class WhatsAppObserver implements IExpenseObserver {
   update(expense: IExpense): void {
-    if (expense.priorityLevel === 'HIGH') {
-      console.log(`📱 WhatsApp enviado: Gasto alto → ${expense.title} S/${expense.amount}`);
-    }
+    console.log(`📱 WhatsApp enviado: GASTOO → ${expense.title} S/${expense.amount}`);
   }
 }
